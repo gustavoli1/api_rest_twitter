73103623765dd7b5976325fce8060c08e99064ec
root <root@gustavo-PC>
2022-01-14 06:02:41 -0300

    Features - Metrics

--- api_rest_twitter/main.py
+++ api_rest_twitter/main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify, abort, make_response, request
+from prometheus_flask_exporter import PrometheusMetrics
 import json, pymysql, os, sys, requests
 app = Flask(__name__)
 app.run(debug=True,port=5000,host='0.0.0.0')
+metrics = PrometheusMetrics(app)
+@app.route('/')
+def main():
+    pass
 @app.route('/insert')
 def getInsert():
+    pass
     BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAPUlXwEAAAAAtzrlqyjGbMw7QvrsxOm57FLVxB8%3DvtN6watQAExUQFq2SlvgHfuMMygcQgoJF1Yo2KYIPMPZvwc3Ld"
     def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
         headers = {"Authorization": "Bearer {}".format(bearer_token)}
@@ -28,12 +34,11 @@ def getInsert():
     return json.dumps(json_response, indent=4, sort_keys=True)
 @app.route('/lang')
 def getCountry():
+    pass
     con = pymysql.connect(host = 'db_twitter',user = 'twitter_user',passwd = 'p0o9i8u7y6',db = 'twitter_db')
     cursor = con.cursor()
     cursor.execute("select hashtag, lang, count(lang) from twitter_db where hashtag='"+request.args["hashtag"]+"' group by lang, hashtag having count(lang) > 1")
     data = cursor.fetchall()
     con.commit()
     con.close()
-    return json.dumps(data, indent=4, sort_keys=True)
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    return json.dumps(data, indent=4, sort_keys=True)    
--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ echo "######################## Inicializando o Ambiente ########################
 
 service docker stop
 
-mv daemon.json /etc/docker/daemon.json
+cp config/daemon.json /etc/docker/daemon.json
 
-service docker restart
+service docker start
 
 docker-compose up -d
+
+docker-compose ps
 
 sleep 30
 
@@ -16,5 +18,5 @@ docker exec -i db_twitter sh -c 'exec mysql -utwitter_user -pp0o9i8u7y6 twitter_
 create table twitter_db (author varchar(280), created_at varchar(280), text text, id varchar(280), author_id varchar(280), lang varchar(5), hashtag varchar(280)); 
 EOF'
 
-docker-compose ps
+
 
