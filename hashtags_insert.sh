617070558385007876920e59b5ddb54db9db6c82
root <root@gustavo-PC>
2022-01-15 11:54:00 -0300

    hashtags_insert

---
+++ hashtags_insert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+hashtags=(openbanking remediation devops sre microservices observability oauth metrics logmonitoring opentracing)
+
+for i in "${hashtags[@]}"
+
+do
+   :
+   time curl "http://localhost:5000/insert?hashtag="+$i
+   echo ""
+   echo "####################   Hashtag $i inserida com sucesso na base de dados!   ###########################"
+   sleep 2
+done
